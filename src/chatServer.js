f47741a17959b0e1f283666db3a80334cbe9b974
Taeyang-Choi <zhfldk1027@gmail.com>
2022-01-21 16:29:14 +0900

    3.2 camera switch

---
+++ src/chatServer.js
@@ -0,0 +1,77 @@
+import express from "express";
+import http from "http";
+import Server from "socket.io";
+import {instrument} from "@socket.io/admin-ui";
+
+const app = express();
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res)=> res.redirect("/"))
+
+const handleListen = () => console.log(`listening on http://localhost:3000, ws://localhost:3000`);
+
+const httpServer = http.createServer(app);
+const wsServer = Server(httpServer, {
+    cors: {
+        origin: ["https://admin.socket.io"],
+        credential: true,
+    }
+});
+
+instrument(wsServer, {
+    auth: false,
+});
+
+function publicRooms() {
+    const {
+        sockets: {
+            adapter: {sids, rooms},
+        }
+    } = wsServer;
+
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+       if (sids.get(key) === undefined) {
+           publicRooms.push(key);
+       }
+    });
+
+    return publicRooms;
+}
+
+function countRoom(roomName) {
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
+wsServer.on("connection", socket => {
+    socket.onAny((event) => {
+    });
+    socket.on("enter_room", (data, done) => {
+        socket.join(data.roomname);
+        socket["nickname"] = data.nickname;
+        done(countRoom(data.roomname));
+        socket.to(data.roomname).emit("welcome", socket.nickname, countRoom(data.roomname));
+        wsServer.sockets.emit("room_change", publicRooms());
+    });
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1));
+    });
+    socket.on("disconnect", () => {
+        wsServer.sockets.emit("room_change", publicRooms());
+    })
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+        done();
+    });
+    socket.on("nickname", nickname => socket["nickname"] = nickname);
+})
+
+httpServer.listen(3000, handleListen);
+
+function makeMessage(type, payload) {
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
